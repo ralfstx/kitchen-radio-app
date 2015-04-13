fe80839e6683f639a26c3fd657dfa1867e7379b0
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-04-13 22:10:05 +0200

    Add cordova build
    Run the cordova build in a directory of its own. The Grunt build copies
    selected source files and npm modules into the cordova directory and
    triggers `cordova build` in this directory.
    
    Exclude cordova plugins, platforms, etc. from repository. Only the
    config.xml needs to reside in the cordova directory.
    
    Call `cordova add platform <directory>` manually, as the platforms are
    manually downloaded from tabrisjs.com.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    clean: ["cordova/www"],
     jshint: {
       options: {
         jshintrc: true
@@ -16,13 +17,29 @@ module.exports = function(grunt) {
         specNameMatcher: "spec"
       },
       all: ["spec/"]
+    },
+    copy: {
+      src: {
+        expand: true,
+        src: ["package.json", "src/**", "node_modules/tabris/**", "node_modules/underscore/**"],
+        dest: "cordova/www/"
+      }
+    },
+    shell: {
+      cordova: {
+        command: ["cd cordova", "cordova build"].join("&&")
+      }
     }
   });
 
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-jasmine-node");
+  grunt.loadNpmTasks("grunt-shell");
 
-  grunt.registerTask("test", "jasmine_node");
-  grunt.registerTask("default", ["jshint", "test"]);
+  grunt.registerTask("test", ["jshint", "jasmine_node"]);
+  grunt.registerTask("build", ["copy", "shell"]);
+  grunt.registerTask("default", ["clean", "test", "build"]);
 
 };
