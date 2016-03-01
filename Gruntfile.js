e3a09661fc60f25e855fabe85809fed308522f68
Ralf Sternberg <rsternberg@eclipsesource.com>
2016-03-01 11:32:04 +0100

    Add babel and serve tasks to Grunt file
    Fix "main" path in "package.json", must be valid in the
    built app. Start server in build/app instead.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,51 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    clean: ["cordova/www"],
+    clean: ["build"],
     copy: {
-      src: {
+      app: {
         expand: true,
-        src: ["package.json", "src/**", "node_modules/tabris/**", "node_modules/underscore/**"],
-        dest: "cordova/www/"
+        src: [
+          "package.json",
+          "src/**",
+          "node_modules/tabris/**",
+          "node_modules/underscore/**",
+          "node_modules/whatwg-fetch/**",
+          "node_modules/babel-polyfill/**"
+        ], //*/
+        dest: "build/app/"
+      },
+      cordova: {
+        expand: true,
+        cwd: "cordova",
+        src: "**",
+        dest: "build/cordova/"
+      }
+    },
+    babel: {
+      app: {
+        options: {
+          presets: ['es2015'],
+          retainLines: true,
+          babelrc: false
+        },
+        files: [{
+          expand: true,
+          cwd: "src/",
+          src: "**/*.js",
+          dest: "build/app/src"
+        }]
       }
     },
     shell: {
+      serve: {
+        command: "hs -c-1",
+        options: {
+          execOptions: {
+            cwd: 'build/app'
+          }
+        }
+      },
       cordova: {
         command: ["cd cordova", "cordova build"].join("&&")
       }
@@ -18,8 +54,13 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks("grunt-contrib-clean");
   grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-babel");
   grunt.loadNpmTasks("grunt-shell");
 
-  grunt.registerTask("default", ["clean", "copy", "shell:cordova"]);
+  grunt.registerTask("default", ["clean", "copy:app", "babel:app"]);
+
+  grunt.registerTask("serve", ["default", "shell:serve"]);
+
+  grunt.registerTask("cordova", ["default", "copy:cordova", "shell:cordova"]);
 
 };
