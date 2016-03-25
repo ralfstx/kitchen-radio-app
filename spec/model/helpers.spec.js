1b20a28f0b4ef677d710b404bf63256dddeb58d1
Ralf Sternberg <rsternberg@eclipsesource.com>
2016-03-25 14:40:42 +0100

    Add helper to apply mixins, use it for Events
    Re-use the Tabris.js built-in Events object as mixin to
    add event support to settings.

--- spec/model/helpers.spec.js
+++ spec/model/helpers.spec.js
@@ -1,4 +1,4 @@
-import { splice } from "../../src/model/helpers.js";
+import { splice, mixin } from "../../src/model/helpers.js";
 
 describe("helpers", function() {
 
@@ -62,6 +62,57 @@ describe("helpers", function() {
         expect(splice([1, 2, 3, 4], 3)).toEqual([[1, 2, 3], [4]]);
       });
 
+    });
+
+  });
+
+  describe("mixin", function() {
+
+    it("throws if target doesn't have prototype", function() {
+      expect(() => mixin({}, {})).toThrow();
+    });
+
+    it("works with plain objects", function() {
+      class Target {}
+      let Mixin = {
+        get foo() { return this._foo * 2; },
+        set foo(value) { this._foo = value + 1; }
+      };
+
+      mixin(Target, Mixin);
+
+      let target = new Target();
+      target.foo = 23;
+      expect(target.foo).toBe(48);
+    });
+
+    it("works with classes", function() {
+      class Target {}
+      class Mixin {
+        get foo() { return this._foo * 2; }
+        set foo(value) { this._foo = value + 1; }
+      }
+
+      mixin(Target, Mixin);
+
+      let target = new Target();
+      target.foo = 23;
+      expect(target.foo).toBe(48);
+    });
+
+    it("skips constructors", function() {
+      class Target {
+        constructor() { this._foo = 23; }
+      }
+      class Mixin {
+        constructor() { this._foo = 24; }
+        get foo() { return this._foo; }
+      }
+
+      mixin(Target, Mixin);
+
+      let target = new Target();
+      expect(target.foo).toBe(23);
     });
 
   });
--- src/model/settings.js
+++ src/model/settings.js
@@ -1,28 +1,26 @@
 import { Events } from "tabris";
+import { mixin } from "./helpers";
 
 const store = {};
 
-function Settings() {
-}
-
-Settings.prototype = {
+class Settings {
 
   get serverUrl() {
     return store.serverUrl;
-  },
+  }
 
   set serverUrl(url) {
     store.serverUrl = url;
     localStorage.setItem("serverUrl", url);
     this.trigger("change:serverUrl");
-  },
+  }
 
   load() {
     store.serverUrl = localStorage.getItem("serverUrl") || "http://192.168.1.";
   }
 
-};
+}
 
-Object.assign(Settings.prototype, Events);
+mixin(Settings, Events);
 
 export default new Settings();
